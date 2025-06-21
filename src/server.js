0b5ebbee8218a067a78493ac606819b1aa6dbcf5
nitinthakurdev <radheradhe9870023@gmail.com>
2025-06-21 22:34:28 +0530

    server runnig

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
+import { json, urlencoded } from 'express';
 import http from 'http';
 
 // server PORT
 
-const SEREVR_PORT = 6000;
+const SEREVR_PORT = 6001;
 
 export function Start(app) {
+  Middlewares(app)
   RoutesHandler(app);
   StartServer(app);
+}
+
+function Middlewares(app) {
+  app.use(json({limit:"10mb"}));
+  app.use(urlencoded({limit:"10mb",extended:true}))
 }
 
 function RoutesHandler(app) {
