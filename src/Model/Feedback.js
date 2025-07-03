b6bf70ffc4927085fa4ca7fdfd36a27044bb85d4
nitinthakurdev <radheradhe9870023@gmail.com>
2025-07-03 22:41:41 +0530

    some change

--- src/Model/Feedback.js
+++ src/Model/Feedback.js
@@ -3,7 +3,11 @@ import { Schema, model } from 'mongoose';
 const FeedBackSchema = new Schema(
   {
     feedback: { type: Boolean, required: true },
-    description: { type: String },
+    theme: { type: String, required: true },
+    environment: { type: String, required: true },
+    participation: { type: String, required: true },
+    audioVideo: { type: String, required: true },
+    additionalFeedback: { type: String },
     class: { type: String, required: true },
     school_code: { type: String, required: true },
     section: { type: String, required: true },
