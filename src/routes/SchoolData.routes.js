ea69cd9bae28677b18360f87853124f4a2fd69e0
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-07-15 11:10:16 +0530

    some change

--- src/routes/SchoolData.routes.js
+++ src/routes/SchoolData.routes.js
@@ -1,14 +1,5 @@
 import { Router } from 'express';
-import {
-  CreateSchoolData,
-  DashboardData,
-  DownloadSchoolData,
-  FilterDataForSchool,
-  FilterSchoolData,
-  getSchoolCodeAndClass,
-  GetSchoolData,
-  GetSchoolSections,
-} from '../controller/SchoolData.controller.js';
+import { CreateSchoolData, DashboardData, DownloadSchoolData, FilterDataForSchool, FilterSchoolData, getSchoolCodeAndClass, GetSchoolData } from '../controller/SchoolData.controller.js';
 import { upload } from '../config/multer.config.js';
 
 const router = Router();
@@ -17,8 +8,7 @@ router.route('/create').post(upload.single('excel'), CreateSchoolData);
 router.route('/get').get(GetSchoolData);
 router.route('/get-filter-data').post(FilterSchoolData);
 router.route('/get-code-class').get(getSchoolCodeAndClass);
-router.route('/get-filtered-section').get(GetSchoolSections);
-router.route('/get-School-data-section').get(FilterDataForSchool);
+router.route('/get-School-data-section').post(FilterDataForSchool);
 router.route('/download-school-data').post(DownloadSchoolData);
 router.route('/dashboard-data').get(DashboardData);
 
